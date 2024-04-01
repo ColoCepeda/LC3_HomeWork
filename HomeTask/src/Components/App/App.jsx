2a809a3b3e08957fa7bf044976f0a923425e4a30
Marcelo <marcelo.cepeda17@gmail.com>
2024-04-01 16:49:57 -0300

    Commit task1 done

--- HomeTask/src/Components/App/App.jsx
+++ HomeTask/src/Components/App/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// Dada la siguiente watchlist, crear una función que tome el array original
+// y devuelva un arreglo de objetos que solo contenga las películas:
+// 1. Dirigidas por Cristopher Nolan.
+// 2. Con un imdbRating > 8.0
+// El arreglo que se devuelve SOLO DEBE CONTENER el nombre de la película, ningún otra información
+// Es obligatorio para este CC resolverlo con la función map()
+import "./App.css";
+import watchList from "./movies";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const returnMovieTitle = (watchlist) => {
+    return watchlist
+      .filter(
+        (movie) =>
+          movie.Director === "Christopher Nolan" &&
+          parseFloat(movie.imdbRating) > 8.0
+      )
+      .map((movie) => ({ Title: movie.Title }));
+  };
+
+  const titlesArray = returnMovieTitle(watchList);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h2>
+        Películas dirigidas por Christopher Nolan con imdbRating `{">"}` 8.0:
+      </h2>
+      <ul>
+        {titlesArray.map((movie, index) => (
+          <li key={index}>{movie.Title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default App
+export default App;
